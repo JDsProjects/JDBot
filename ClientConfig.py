169ab64b48c3794a46b2be8fe001184b6097fd97
JDJG Inc. Official <ghostzap123@gmail.com>
2020-12-21 20:13:42 +0000

    Added some commands

--- ClientConfig.py
+++ ClientConfig.py
@@ -1,5 +1,13 @@
 import discord
+import re
 from discord.ext import commands
-client = discord.Client(intents = discord.Intents.all())
-discordprefix = "test*"
-client = commands.Bot(command_prefix=commands.when_mentioned_or(discordprefix))
+
+async def get_prefix(client,message):
+  extras = ["test*"]
+  comp = re.compile("^(" + "|".join(map(re.escape, extras)) + ").*", flags=re.I)
+  match = comp.match(message.content)
+  if match is not None:
+    extras.append(match.group(1))
+  return commands.when_mentioned_or(*extras)(client, message)
+
+client = commands.Bot(command_prefix=(get_prefix),intents = discord.Intents.all())
--- main.py
+++ main.py
@@ -1,15 +1,44 @@
 import discord
 from discord.ext import commands
 import math
+import discord.utils
 import B
 import ClientConfig
 import os
 import random
-import userinfo
+import logging
+import json
 import aiohttp
+import typing
+import re
+import asyncio
 
-discordprefix = "test*"
+logging.basicConfig(level=logging.WARNING)
+ratelimit_detection=logging.Filter(name='WARNING:discord.http:We are being rate limited.')
+
 client = ClientConfig.client
+
+class BetterUserconverter(commands.Converter):
+  async def convert(self, ctx, argument):
+    user=ctx.guild.get_member_named(argument)
+    if user == None:
+      match = re.compile(r'([0-9]{15,21})$').match(argument) or re.match(r'<@!?([0-9]+)>$', argument)
+      if match:
+        argument = match.group(1)
+
+      if not match:
+        match2 = re.match(r'<@&([0-9]+)>$',argument)
+        if match2:
+          print(match2.group(1))
+
+      if argument.isdigit() == True:
+        user=client.get_user(int(argument))
+        if user == None:
+          try:
+            user=await client.fetch_user(int(argument))
+          except:
+            user = None
+    return user
 
 @client.command()
 async def ping(ctx):
@@ -20,55 +49,47 @@ async def ping(ctx):
 async def pi(ctx):
   await ctx.send(math.pi)
 
-@client.command()
-async def hug(ctx):
-  message = ctx.message
-  hug = message.content.replace(discordprefix+"hug","")
-  if hug == (""):
-    person = client.user
-    target = message.author
-  
-  if len(message.mentions) > 0:
-    target = message.mentions[0]
-    person = message.author
-  
-  if len(message.mentions) == 0:
-    person = message.author
-    target=await userinfo.user_grab(message)
-
-  if target.id == person.id:
-    person = client.user
-    target = message.author
-  async with aiohttp.ClientSession() as cs:
-    async with cs.get('https://some-random-api.ml/animu/hug') as anime:
-      res = await anime.json()
-  for x in res:
-    embed=discord.Embed(color=random.randint(0, 16777215))
-    embed.set_author(name=f"{person} hugged you",icon_url=(person.avatar_url))
-    embed.set_image(url=res[x])
-    await message.channel.send(content=target.mention,embed=embed)
-
-
 @client.event
 async def on_ready():
-  print("Bot is online")
+  print("Bot is Ready")
   print(f"Logged in as {client.user}")
   print(f"Id: {client.user.id}")
+
+@client.command(help="repeats what you say",brief="a command that repeats what you say the orginal message is deleted")
+async def say(ctx,*,args=None):
+  if args:
+    await ctx.send(args)
+
+@client.command(help="a command that gives information on users",brief="this can work with mentions, ids, usernames, and even full names.")
+async def userinfo(ctx,user: BetterUserconverter=None):
+  if user == None:
+    user = ctx.author
+  await ctx.send(user)
 
 @client.event
 async def on_message(message):
   if isinstance(message.channel, discord.DMChannel):
-    print("DM channel")
+    time_used=(message.created_at).strftime('%m/%d/%Y %H:%M:%S')
+    embed_message = discord.Embed(title=message.content, description=time_used, color=random.randint(0, 16777215))
+    embed_message.set_author(name=f"Direct Message From {message.author}:",icon_url=(message.author.avatar_url))
+    embed_message.set_footer(text = f"{message.author.id}")
+    embed_message.set_thumbnail(url = "https://media.discordapp.net/attachments/556242984241201167/763866804359135292/inbox.png?width=677&height=677")
+    channel_usage=client.get_channel(738912143679946783)
+    embed_message.add_field(name="Sent To:",value=str(channel_usage))
+    await channel_usage.send(embed=embed_message)
   
   test=await client.get_context(message)
   if (test.valid) == False:
-    pass
-  print(test.prefix)
+    if test.prefix != None:
+      if test.command == None:
+        time_used=(message.created_at).strftime('%m/%d/%Y %H:%M:%S')
+        embed_message = discord.Embed(title=f" {message.content}", description=time_used,color=random.randint(0, 16777215))
+        embed_message.set_author(name=f"{message.author} tried to excute invalid command:",icon_url=(message.author.avatar_url))
+        embed_message.set_footer(text = f"{message.author.id}")
+        embed_message.set_thumbnail(url="https://media.discordapp.net/attachments/738912143679946783/763873708908478474/Warning.png")
+        await client.get_channel(738912143679946783).send(embed=embed_message)
   
   await client.process_commands(message)
-
-
-
 
   
 
