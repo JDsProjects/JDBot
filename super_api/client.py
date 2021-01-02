1002625210251c5faa5de5a624a53802bf8de245
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-02 04:12:40 +0000

    adding a module for JDBot

---
+++ super_api/client.py
@@ -0,0 +1,19 @@
+import aiohttp
+from .http import HTTPClient
+from yarl import URL
+
+class Client:
+  def __init__(self,session: aiohttp.ClientSession = None):
+    self._http_client = HTTPClient(session)
+
+  def super_api_url(self,endpoint):
+    url = URL.build(
+      scheme: "https",
+      host: "asuna.ga/api",
+      path=endpoint
+    )
+
+    return str(url)
+
+  async def close(self):
+      await self._http_client.close()
---
+++ super_api/readme.md
@@ -0,0 +1,2 @@
+Some of the code came from https://github.com/iDutchy/sr_api/
+(Since I didn't know how to build my own wrapper)
