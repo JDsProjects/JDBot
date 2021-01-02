b283441639c2daa0deaf7da3a74a7e89192be600
JDJG Inc. Official <ghostzap123@gmail.com>
2021-01-02 22:30:52 +0000

    aioimgur was sadly removed :(

--- aioimgur/helpers/__init__.py
+++
@@ -1,4 +0,0 @@
-from ..imgur.models.comment import Comment
-from ..imgur.models.notification import Notification
-from ..imgur.models.gallery_album import GalleryAlbum
-from ..imgur.models.gallery_image import GalleryImage
--- aioimgur/imgur/models/tag_vote.py
+++
@@ -1,7 +0,0 @@
-class TagVote(object):
-
-    def __init__(self, ups, downs, name, author):
-        self.ups = ups
-        self.downs = downs
-        self.name = name
-        self.author = author
