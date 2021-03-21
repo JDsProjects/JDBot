424a7ae85b582198951b342369c92bbdc726b3c2
Shadi Alostaz <66328589+SilentSerenityy@users.noreply.github.com>
2021-03-21 12:34:50 -0400

    Update the presence of the bot.
    The status should change more frequently because 40 seconds is way too long.
    Idle status will match the bot's yellow pfp

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import DatabaseControl
 
 async def status_task():
   while True:
-    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name="the return of JDBot"))
-    await asyncio.sleep(40)
-    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(client.guilds)} servers | {len(client.users)} users"))
-    await asyncio.sleep(40)
-    await client.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="the new updates coming soon..."))
-    await asyncio.sleep(40)
+    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="the return of JDBot"))
+    await asyncio.sleep(10)
+    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(client.guilds)} servers | {len(client.users)} users"))
+    await asyncio.sleep(10)
+    await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="the new updates coming soon..."))
+    await asyncio.sleep(10)
 
 async def startup():
   await client.wait_until_ready()
