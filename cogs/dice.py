e9fde68702a879c7354b29baed0b83bd1e4cc739
JDJG Inc. Official <>
2021-05-17 20:54:55 +0000

    Updated Diceroll cog to be a bit cleaner, needs a bit more clean up though.

--- cogs/dice.py
+++ cogs/dice.py
@@ -6,68 +6,29 @@ class Dice(commands.Cog):
   def __init__(self,client):
     self.client = client
 
-  @commands.command(brief="a command to roll d20",aliases=["roll20"])
-  async def dice_roll20(self,ctx):
-    embed_message = discord.Embed(title=f" Rolled a {random.randint(1,20)}", color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
-    embed_message.set_footer(text = f"{ctx.author.id}")
-    embed_message.set_thumbnail(url="https://i.imgur.com/AivZBWP.png")
-    embed_message.set_author(name=f"d20 Rolled by {ctx.author}:",icon_url=(ctx.author.avatar_url))
-    embed_message.set_image(url="https://i.imgur.com/9dbBkqj.gif")
-    await ctx.send(embed=embed_message)
+  async def generate_embed(self,ctx,number):
+    if str(number) is "20": url = "https://i.imgur.com/9dbBkqj.gif"
+    if str(number) is "6": url = "https://i.imgur.com/6ul8ZGY.gif"
+    else: url = "https://i.imgur.com/gaLM6AG.gif"
 
-  @commands.command(brief="a command to roll d6",aliases=["roll6"])
-  async def dice_roll6(self,ctx):
-    embed_message = discord.Embed(title=f" Rolled a {random.randint(1,6)}", color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
-    embed_message.set_footer(text = f"{ctx.author.id}")
-    embed_message.set_thumbnail(url="https://i.imgur.com/AivZBWP.png")
-    embed_message.set_author(name=f"d6 Rolled by {ctx.author}:",icon_url=(ctx.author.avatar_url))
-    embed_message.set_image(url="https://i.imgur.com/6ul8ZGY.gif")
-    await ctx.send(embed=embed_message)
+    if number < 1:  
+      await ctx.send("NO")
+      return
 
-  @commands.command(brief="a command to roll d10",aliases=["roll10"])
-  async def dice_roll10(self,ctx):
-    embed_message = discord.Embed(title=f" Rolled a {random.randint(1,10)}", color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
-    embed_message.set_footer(text = f"{ctx.author.id}")
-    embed_message.set_thumbnail(url="https://i.imgur.com/AivZBWP.png")
-    embed_message.set_author(name=f"d10 Rolled by {ctx.author}:",icon_url=(ctx.author.avatar_url))
-    embed_message.set_image(url="https://i.imgur.com/gaLM6AG.gif")
-    await ctx.send(embed=embed_message)
+    embed = discord.Embed(title=f" Rolled a {random.randint(1,number)}", color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
+    embed.set_footer(text = f"{ctx.author.id}")
+    embed.set_thumbnail(url="https://i.imgur.com/AivZBWP.png")
+    embed.set_author(name=f"d{number} Rolled by {ctx.author}:",icon_url=(ctx.author.avatar_url))
+    embed.set_image(url=url)
+    await ctx.send(embed=embed)
 
-  @commands.command(brief="a command to roll d100",aliases=["roll100"])
-  async def dice_roll100(self,ctx):
-    embed_message = discord.Embed(title=f" Rolled a {random.randint(1,100)}", color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
-    embed_message.set_footer(text = f"{ctx.author.id}")
-    embed_message.set_thumbnail(url="https://i.imgur.com/AivZBWP.png")
-    embed_message.set_author(name=f"d100 Rolled by {ctx.author}:",icon_url=(ctx.author.avatar_url))
-    embed_message.set_image(url="https://i.imgur.com/gaLM6AG.gif")
-    await ctx.send(embed=embed_message)
-
-  @commands.command(brief="a command to roll d12",aliases=["roll12"])
-  async def dice_roll12(self,ctx):
-    embed_message = discord.Embed(title=f" Rolled a {random.randint(1,12)}", color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
-    embed_message.set_footer(text = f"{ctx.author.id}")
-    embed_message.set_thumbnail(url="https://i.imgur.com/AivZBWP.png")
-    embed_message.set_author(name=f"d12 Rolled by {ctx.author}:",icon_url=(ctx.author.avatar_url))
-    embed_message.set_image(url="https://i.imgur.com/gaLM6AG.gif")
-    await ctx.send(embed=embed_message)
-
-  @commands.command(brief="a command to roll d8",aliases=["roll8"])
-  async def dice_roll8(self,ctx):
-    embed_message = discord.Embed(title=f" Rolled a {random.randint(1,8)}", color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
-    embed_message.set_footer(text = f"{ctx.author.id}")
-    embed_message.set_thumbnail(url="https://i.imgur.com/AivZBWP.png")
-    embed_message.set_author(name=f"d8 Rolled by {ctx.author}:",icon_url=(ctx.author.avatar_url))
-    embed_message.set_image(url="https://i.imgur.com/gaLM6AG.gif")
-    await ctx.send(embed=embed_message)
-
-  @commands.command(brief="a command to roll d4",aliases=["roll4"])
-  async def dice_roll4(self,ctx):
-    embed_message = discord.Embed(title=f" Rolled a {random.randint(1,4)}", color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
-    embed_message.set_footer(text = f"{ctx.author.id}")
-    embed_message.set_thumbnail(url="https://i.imgur.com/AivZBWP.png")
-    embed_message.set_author(name=f"d4 Rolled by {ctx.author}:",icon_url=(ctx.author.avatar_url))
-    embed_message.set_image(url="https://i.imgur.com/gaLM6AG.gif")
-    await ctx.send(embed=embed_message)
+  @commands.command(brief="A command to roll random dnd dice.",aliases=["roll"])
+  async def diceroll(self,ctx,number=None):
+    if number:
+      if number.isdigit(): await self.generate_embed(ctx,int(number))
+      else: number = None
+    if number is None:
+      await ctx.send("None that's bad.")
 
   @commands.command(brief="Gives random emojis(from guild and bot)",help="Please use wisely.",aliases=["e_spin","emoji_spin"])
   async def emoji_spinner(self,ctx):
