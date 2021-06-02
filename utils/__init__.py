2d9947f68ec4e114334589acb75b425fa94e3512
JDJG Inc. Official <>
2021-06-02 05:29:57 +0000

    Updated all utils to use * rather than manually doing from , for stuff.

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
-from .converters import BetterMemberConverter, BetterUserconverter, guildinfo, EmojiBasic, EmojiConverter, check
-from .extra import triggered_converter, headpat_converter, warn_permission, invert_converter, headpat_converter2, google_tts
-from .image import invert_func
-from .menus import InviteInfoEmbed, EmojiInfoEmbed
+from .converters import *
+from .extra import *
+from .image import *
+from .menus import *
