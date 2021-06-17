a8161300a3d09a3671bf52bb0b5ea2f1dcf22ba7
JDJG Inc. Official <ghostzap123@gmail.com>
2021-06-17 19:27:57 +0000

    Updated and added some new stuff lol :D

--- cogs/image.py
+++ cogs/image.py
@@ -7,7 +7,7 @@ class Image(commands.Cog):
     self.client = client
 
   @commands.command(brief="a command to slap someone",help="this sends a slap gif to the target user")
-  async def slap(self,ctx,*, Member: utils.BetterMemberConverter = None):
+  async def slap(self, ctx, *, Member: utils.BetterMemberConverter = None):
     Member = Member or ctx.author
       
     if Member.id == ctx.author.id:
@@ -145,7 +145,8 @@ class Image(commands.Cog):
         await ctx.author.send("Failed DM'ing them...")
   
   @commands.command(help="takes a .png attachment or your avatar and makes a triggered version.")
-  async def triggered(self,ctx):
+  async def triggered(self, ctx, *, Member: utils.BetterMemberConverter = None):
+    Member = Member or ctx.author
     y = 0
     if ctx.message.attachments:
       for x in ctx.message.attachments:
@@ -157,11 +158,12 @@ class Image(commands.Cog):
           pass
 
     if not ctx.message.attachments or y == 0:
-      url = ctx.author.avatar_url_as(format="png")
+      url = Member.avatar_url_as(format="png")
       await utils.triggered_converter(self,url,ctx)
 
   @commands.command(brief="uses our headpat program to pat you",help="a command that uses sra_api to make a headpat of you.")
-  async def headpat2(self,ctx):
+  async def headpat2(self, ctx, *, Member: utils.BetterMemberConverter = None):
+    Member = Member or ctx.author
     y = 0
     if ctx.message.attachments:
       for x in ctx.message.attachments:
@@ -173,11 +175,11 @@ class Image(commands.Cog):
           pass
 
     if not ctx.message.attachments or y == 0:
-      url = ctx.author.avatar_url_as(format="png")
-      await utils.headpat_converter(self,url,ctx)
+      url = Member.avatar_url_as(format="png")
+      await utils.headpat_converter(self, url, ctx)
 
   @commands.command(brief="a hug command to hug people",help="this actually the second hug command and is quite powerful.")
-  async def hug2(self,ctx,*, Member: utils.BetterMemberConverter=None):
+  async def hug2(self, ctx, *, Member: utils.BetterMemberConverter=None):
     Member = Member or ctx.author
       
     if Member.id == ctx.author.id:
@@ -209,7 +211,7 @@ class Image(commands.Cog):
         await ctx.author.send("Failed DM'ing them...")
 
   @commands.command(brief="a kiss command",help="a command where you can target a user or pick yourself to get a kiss gif( I don't know why I have this)")
-  async def kiss(self,ctx,*, Member: utils.BetterMemberConverter=None):
+  async def kiss(self, ctx, *, Member: utils.BetterMemberConverter=None):
     Member = Member or ctx.author
       
     if Member.id == ctx.author.id:
@@ -241,7 +243,7 @@ class Image(commands.Cog):
         await ctx.author.send("Failed Dming them...")
 
   @commands.command(brief="a command to get a neko",help="using the asuna.ga api you will get these images")
-  async def neko(self,ctx):
+  async def neko(self, ctx):
     asuna = asuna_api.Client(session=self.client.session)
     url = await asuna.get_gif("neko")
     
@@ -252,7 +254,7 @@ class Image(commands.Cog):
     await ctx.send(embed=embed)
 
   @commands.command(brief="a command to send wink gifs",wink="you select a user to send it to and it will send it to you lol")
-  async def wink(self,ctx,*, Member: utils.BetterMemberConverter=None):
+  async def wink(self, ctx, *, Member : utils.BetterMemberConverter = None):
     Member = Member or ctx.author
       
     if Member.id == ctx.author.id:
@@ -284,7 +286,7 @@ class Image(commands.Cog):
         await ctx.author.send("Failed Dming them...")
 
   @commands.command(brief="Gives you a random waifu image.")
-  async def waifu(self,ctx):
+  async def waifu(self, ctx):
     r=await self.client.session.get('https://api.waifu.pics/sfw/waifu')
     res = await r.json()
     embed=discord.Embed(color=random.randint(0, 16777215),timestamp=(ctx.message.created_at))
@@ -304,7 +306,7 @@ class Image(commands.Cog):
     await ctx.send(embed=embed)
 
   @commands.command(brief="a command to send facepalm gifs",help="using some random api it sends you a facepalm gif lol")
-  async def facepalm(self,ctx,*, Member: utils.BetterMemberConverter=None):
+  async def facepalm(self, ctx, *, Member: utils.BetterMemberConverter=None):
     Member = Member or ctx.author
       
     if Member.id == ctx.author.id:
@@ -336,7 +338,7 @@ class Image(commands.Cog):
         await ctx.author.send("Failed Dming them...")
 
   @commands.command(help="gives a random objection",aliases=["obj","ob","object"])
-  async def objection(self,ctx):
+  async def objection(self ,ctx):
     r=await self.client.session.get('https://jdjgapi.nom.mu/api/objection')
     res = await r.json()
     embed = discord.Embed(color=random.randint(0, 16777215))
@@ -346,7 +348,7 @@ class Image(commands.Cog):
     await ctx.send(embed=embed)
 
   @commands.command(help="gives the truth about opinions(may offend)",aliases=["opinion"])
-  async def opinional(self,ctx):
+  async def opinional(self, ctx):
     r=await self.client.session.get('https://jdjgapi.nom.mu/api/opinional')
     res = await r.json()
     embed = discord.Embed(title = "Truth about opinions(may offend some people):",color=random.randint(0, 16777215))
@@ -355,38 +357,40 @@ class Image(commands.Cog):
     await ctx.send(embed=embed)
 
   @commands.command(brief="a command to send I hate spam.")
-  async def spam(self,ctx):
+  async def spam(self, ctx):
     embed=discord.Embed(color=random.randint(0, 16777215))
     embed.set_image(url="https://i.imgur.com/1LckTTu.gif")
     await ctx.send(content="I hate spam.",embed=embed)
 
   @commands.command(brief="gives you the milkman gif",help="you summoned the milkman oh no")
-  async def milk(self,ctx):
+  async def milk(self, ctx):
     embed = discord.Embed(title="You have summoned the milkman",color=random.randint(0, 16777215))
     embed.set_image(url="https://i.imgur.com/JdyaI1Y.gif")
     embed.set_footer(text="his milk is delicious")
     await ctx.send(embed=embed)
 
   @commands.command(help="inverts any valid image within the sr_api")
-  async def invert2(self,ctx):
+  async def invert2(self, ctx, *, Member: utils.BetterMemberConverter = None):
+    Member = Member or ctx.author
     y = 0
-    if len(ctx.message.attachments) > 0:
+    if ctx.message.attachments:
       for x in ctx.message.attachments:
         if x.filename.endswith(".png") or x.filename.endswith(".jpg"):
           url = x.url
-          await utils.invert_converter(url,ctx)
+          await utils.invert_converter(self, url, ctx)
           y += 1
         if not x.filename.endswith(".png") or not x.filename.endswith(".jpg"):
           pass
 
-    if len(ctx.message.attachments) == 0 or y == 0:
-      url = ctx.author.avatar_url_as(format="png")
-      await utils.invert_converter(self,url,ctx)
+    if not ctx.message.attachments or y == 0:
+      url = Member.avatar_url_as(format="png")
+      await utils.invert_converter(self, url, ctx)
 
   @commands.command(help="Headpat generator :D")
-  async def headpat(self,ctx):
+  async def headpat(self, ctx, Member: utils.BetterMemberConverter = None):
+    Member = Member or ctx.author
     y = 0
-    if len(ctx.message.attachments) > 0:
+    if ctx.message.attachments:
       for x in ctx.message.attachments:
         if x.filename.endswith(".png") or x.filename.endswith(".jpg"):
           url = x.proxy_url
@@ -395,9 +399,9 @@ class Image(commands.Cog):
         if not x.filename.endswith(".png") or not x.filename.endswith(".jpg"):
           pass
 
-    if len(ctx.message.attachments) == 0 or y == 0:
-      url = ctx.author.avatar_url_as(format="png")
-      await utils.headpat_converter2(self,url,ctx)
+    if not ctx.message.attachments or y == 0:
+      url = Member.avatar_url_as(format="png")
+      await utils.headpat_converter2(self, url, ctx)
 
   def convert_svg(self, svg_image):
     converted_bytes = cairosvg.svg2png(bytestring = svg_image)
