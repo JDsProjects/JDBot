db4e97250e5d5968d6599b1e9127af29c71ebd72
JDJG Inc. Official <ghostzap123@gmail.com>
2021-07-07 02:47:45 +0000

    ok

--- ClientConfig.py
+++ ClientConfig.py
@@ -21,13 +21,11 @@ class JDBot(commands.Bot):
   async def start(self,*args, **kwargs):
     self.session=aiohttp.ClientSession()
     self.sus_users = await aiosqlite.connect('sus_users.db')
-    self.rtfm = await aiosqlite.connect('rtfm.db')
     await super().start(*args, **kwargs)
 
   async def close(self):
     await self.session.close()
     await self.sus_users.close()
-    await self.rtfm.close()
     await super().close()
 
   async def getch_member(self, guild, member_id):
