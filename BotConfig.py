f0342291b9b359c2fcd89cc3cb147b82e5736cbe
JDJG Inc. Official <ghostzap123@gmail.com>
2021-07-12 20:33:54 +0000

    Okay dookie

--- BotConfig.py
+++ BotConfig.py
@@ -62,7 +62,7 @@ class JDBot(commands.Bot):
         
     return [cmd for cmd in command_list if await check(cmd, ctx)]
 
-bot = JDBot(command_prefix = (get_prefix), intents = intents,chunk_guilds_at_startup = False, strip_after_prefix = True)
+bot = JDBot(command_prefix = (get_prefix), intents = intents, chunk_guilds_at_startup = False, strip_after_prefix = True)
 
 bot.launch_time = datetime.datetime.utcnow()
 
