c3846acf5ffbc159397c9b770fad8da9baabd5eb
JDJG Inc. Official <ghostzap123@gmail.com>
2021-08-05 20:28:44 +0000

    gifs don't really work yet

--- utils/image.py
+++ utils/image.py
@@ -6,23 +6,24 @@ def invert_func(bytes_returned):
   image = Image.open(io.BytesIO(bytes_returned))
   if image.mode == 'RGBA':
     r,g,b,a = image.split()
-    rgb_image = Image.merge('RGB', (r,g,b))
+    rgb_image = Image.merge('RGB', (r, g, b))
 
     inverted_image = ImageOps.invert(rgb_image)
 
     r2,g2,b2 = inverted_image.split()
 
-    final_transparent_image = Image.merge('RGBA', (r2,g2,b2,a))
+    final_transparent_image = Image.merge('RGBA', (r2, g2, b2, a))
 
     buffer = io.BytesIO()
     final_transparent_image.save(buffer, image.format)
     buffer.seek(0)
-    file = discord.File(buffer, filename=f"inverted.{image.format}")
+    file = discord.File(buffer, filename = f"inverted.{image.format}")
     return file
 
   else:
     try:
       inverted_image = ImageOps.invert(image)
+      
     except NotImplementedError:
       buffer = io.BytesIO(bytes_returned)
       file = discord.File(buffer, filename = f"failed.{image.format}")
@@ -31,5 +32,5 @@ def invert_func(bytes_returned):
     buffer = io.BytesIO()
     inverted_image.save(buffer, image.format, save_all = True)
     buffer.seek(0)
-    file = discord.File(buffer ,filename = f"inverted.{image.format}")
+    file = discord.File(buffer, filename = f"inverted.{image.format}")
     return file
