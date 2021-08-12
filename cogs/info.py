04574aa374557ede6629e7134a8ce39685f283ea
JDJG Inc. Official <ghostzap123@gmail.com>
2021-08-12 22:21:30 +0000

    A lot of stuff here for well paginator stuff.

--- cogs/info.py
+++ cogs/info.py
@@ -79,22 +79,17 @@ class Info(commands.Cog):
     embed.set_image(url = user.avatar.url)
     
     view = utils.BasicButtons(ctx.author)
-    msg = await ctx.send("do you want the mutual guilds to be dmed to you?", embed = embed, view = view)
+    await ctx.send("do you want the mutual guilds to be dmed or secretly sent to you?(both will require more buttons to be hit)", embed = embed, view = view)
 
     await view.wait()
 
     if view.value is None:
-      await msg.delete()
       return await ctx.send("you didn't respond quickly enough")
 
     if not view.value:
-      await msg.delete()
-      await ctx.send("Not dming the mutual guilds list to you.")
+      await ctx.send("Not sending the mutual guilds list to you.")
 
     if view.value:
-      await msg.delete()
-      await ctx.send("Dming you the mutual guilds list")
-
       pag = commands.Paginator()
 
       guilds_list = [guild for guild in self.bot.guilds if guild.get_member(user.id) and guild.get_member(ctx.author.id)]
@@ -105,12 +100,16 @@ class Info(commands.Cog):
       pages = [page.strip("`") for page in pag.pages]
       pages = pages or ["None"]
 
-      menu = ViewMenuPages(utils.mutualGuildsEmbed(pages, per_page = 1),delete_message_after=True)
+      view = utils.dm_or_ephemeral(ctx.author, pages)
 
-      if (ctx.author.dm_channel is None):
-        await ctx.author.create_dm()
-        
-      await menu.start(ctx, channel = ctx.author.dm_channel)
+      await ctx.send("Opening another buttons response.", view = view)
+
+      #menu = ViewMenuPages(utils.mutualGuildsEmbed(pages, per_page = 1), delete_message_after = True)
+
+      #if (ctx.author.dm_channel is None):
+        #await ctx.author.create_dm()
+
+      #await menu.start(ctx, channel = ctx.author.dm_channel)
 
   async def cog_command_error(self, ctx, error):
     if ctx.command or not ctx.command.has_error_handler():
