65d58fa4467c642ace8347146b0e9ef49081da71
JDJG Inc. Official <ghostzap123@gmail.com>
2021-08-13 19:23:33 +0000

    updated this to be more accurate.

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -9,18 +9,18 @@ class Events(commands.Cog):
   async def on_guild_join(self, guild):
     channels = [channel for channel in guild.channels]
     roles = roles= [role for role in guild.roles]
-    embed = discord.Embed(title="Bot just joined: "+str(guild.name), color=random.randint(0,16777215))
+    embed = discord.Embed(title = f"Bot just joined : {guild.name}", color=random.randint(0,16777215))
     
     embed.set_thumbnail(url = guild.icon.url if guild.icon else "https://i.imgur.com/3ZUrjUP.png")
 
     embed.add_field(name='Server Name:',value=f'{guild.name}')
     embed.add_field(name='Server ID:',value=f'{guild.id}')
     embed.add_field(name='Server region:',value=f'{guild.region}')
-    embed.add_field(name='Server Creation Date:',value=f'{guild.created_at}')
+    embed.add_field(name='Server Creation Date:', value=f"{discord.utils.format_dt(guild.created_at, style = 'd')}\n{discord.utils.format_dt(guild.created_at, style = 'T')}")
     embed.add_field(name='Server Owner:',value=f'{guild.owner}')
     embed.add_field(name='Server Owner ID:',value=f'{guild.owner_id}')
     embed.add_field(name='Member Count:',value=f'{guild.member_count}')
-    embed.add_field(name='Amount of Channels:',value=f"{len(channels)}")
+    embed.add_field(name='Amount of Channels:', value=f"{len(channels)}")
     embed.add_field(name='Amount of Roles:',value=f"{len(roles)}")
     await self.bot.get_channel(855217084710912050).send(embed=embed)
 
@@ -28,14 +28,14 @@ class Events(commands.Cog):
   async def on_guild_remove(self, guild):
     channels = [channel for channel in guild.channels]
     roles = roles= [role for role in guild.roles]
-    embed = discord.Embed(title="Bot just left: "+str(guild.name), color=random.randint(0,16777215))
+    embed = discord.Embed(title = f"Bot just left : {guild.name}", color = random.randint(0,16777215))
     
     embed.set_thumbnail(url = guild.icon.url if guild.icon else "https://i.imgur.com/3ZUrjUP.png")
 
-    embed.add_field(name='Server Name:',value=f'{guild.name}')
-    embed.add_field(name='Server ID:',value=f'{guild.id}')
-    embed.add_field(name='Server region:',value=f'{guild.region}')
-    embed.add_field(name='Server Creation Date:',value=f'{guild.created_at}')
+    embed.add_field(name = 'Server Name:',value=f'{guild.name}')
+    embed.add_field(name = 'Server ID:',value=f'{guild.id}')
+    embed.add_field(name = 'Server region:', value=f'{guild.region}')
+    embed.add_field(name='Server Creation Date:', value=f"{discord.utils.format_dt(guild.created_at, style = 'd')}\n{discord.utils.format_dt(guild.created_at, style = 'T')}")
     embed.add_field(name='Server Owner:',value=f'{guild.owner}')
     embed.add_field(name='Server Owner ID:',value=f'{guild.owner_id}')
     try:
