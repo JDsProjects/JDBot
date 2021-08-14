ad920eb1d1462f9d859f0c048c3b76656019afe7
JDJG Inc. Official <ghostzap123@gmail.com>
2021-08-14 01:11:46 +0000

    updated jishaku to have a speacil cog_command_eror now it can send errors which means you screw something up then all is good

--- cogs/jsk.py
+++ cogs/jsk.py
@@ -5,7 +5,13 @@ from jishaku.cog import STANDARD_FEATURES, OPTIONAL_FEATURES
 #look into making more jishaku commands: https://jishaku.readthedocs.io/en/latest/cog.html
 
 class Jishaku(*OPTIONAL_FEATURES, *STANDARD_FEATURES):
-  pass
+  async def cog_command_error(self, ctx, error):
+    if ctx.command and not ctx.command.has_error_handler():
+      await ctx.send(error)
+      import traceback
+      traceback.print_exc()
+      
+    #I need to fix all cog_command_error
 
 def setup(bot: commands.Bot):
   bot.add_cog(Jishaku(bot=bot))
