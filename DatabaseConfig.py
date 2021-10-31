7e1df3b7788603b0954674e7e920dcaf8b8070eb
JDJG Inc. Official <ghostzap123@gmail.com>
2021-10-31 22:32:21 +0000

    removed job data because sql will handle that.

--- DatabaseConfig.py
+++ DatabaseConfig.py
@@ -6,5 +6,4 @@ DB_client = motor.motor_asyncio.AsyncIOMotorClient(DB_logindetails)
 db = DB_client.JDBot_data
 content_database = db.list_collection_names()
 db.money_system.create_index([('user_id', pymongo.ASCENDING)],unique=True)
-db.job_listing.create_index([('job_id', pymongo.ASCENDING)],unique=True)
 db.spy_co_data.create_index([('game_id', pymongo.ASCENDING)],unique=True)
