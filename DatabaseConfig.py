5ccf034306d0a23bd363aa677339570be6b81c86
JDJG Inc. Official <ghostzap123@gmail.com>
2021-10-31 22:31:55 +0000

    dropped guild prefixes(will be in sql soon, as an addon to the normal prefixes i have)

--- DatabaseConfig.py
+++ DatabaseConfig.py
@@ -6,6 +6,5 @@ DB_client = motor.motor_asyncio.AsyncIOMotorClient(DB_logindetails)
 db = DB_client.JDBot_data
 content_database = db.list_collection_names()
 db.money_system.create_index([('user_id', pymongo.ASCENDING)],unique=True)
-db.guild_prefixes.create_index([('ser_id', pymongo.ASCENDING)],unique=True)
 db.job_listing.create_index([('job_id', pymongo.ASCENDING)],unique=True)
 db.spy_co_data.create_index([('game_id', pymongo.ASCENDING)],unique=True)
