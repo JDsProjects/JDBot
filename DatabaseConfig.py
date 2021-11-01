d82d8807d625424dbeb209a0295192775ffafc03
JDJG Inc. Official <ghostzap123@gmail.com>
2021-11-01 06:32:02 +0000

    ok...

--- DatabaseConfig.py
+++ DatabaseConfig.py
@@ -6,4 +6,4 @@ DB_client = motor.motor_asyncio.AsyncIOMotorClient(DB_logindetails)
 db = DB_client.JDBot_data
 content_database = db.list_collection_names()
 
-db.money_system.create_index([('user_id', pymongo.ASCENDING)],unique=True)
+db.money_system.create_index([('user_id', pymongo.ASCENDING)],unique = True)
