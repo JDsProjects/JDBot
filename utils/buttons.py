cea9dc37f495a3ff3a861fbbccef08220516633b
JDJG Inc. Official <ghostzap123@gmail.com>
2021-11-05 03:03:45 +0000

    check time.

--- utils/buttons.py
+++ utils/buttons.py
@@ -1,22 +1,13 @@
 import discord, typing, asyncio
 
 class BasicButtons(discord.ui.View):
-  def __init__(self, authorized_user: typing.Union[discord.User, discord.Member] = None, **kwargs):
+  def __init__(self, ctx, **kwargs):
     super().__init__(**kwargs)
-    self.authorized_user = authorized_user
+    self.ctx = ctx
     self.value: str = None
-  
-  def __authorized__(self, button: discord.ui.Button, interaction: discord.Interaction) -> bool:
-    if self.authorized_user and self.authorized_user.id != interaction.user.id:
-      return False
-
-    return True
 
   @discord.ui.button(label = "Accept", style = discord.ButtonStyle.success, emoji = "✅")
   async def accept(self, button: discord.ui.Button, interaction: discord.Interaction):
-    if not self.__authorized__(button, interaction):
-
-      return await interaction.response.send_message(content = f"You Can't Use that button, {self.authorized_user.mention} is the author of this message.", ephemeral = True)
 
     self.clear_items()
     await interaction.response.edit_message(view = self)
@@ -25,13 +16,18 @@ class BasicButtons(discord.ui.View):
 
   @discord.ui.button(label="Deny", style = discord.ButtonStyle.danger , emoji = "❌")
   async def denied(self, button: discord.ui.Button, interaction: discord.Interaction):
-    if not self.__authorized__(button, interaction):
-      return await interaction.response.send_message(content = f"You Can't Use that button, {self.authorized_user.mention} is the author of this message.", ephemeral = True)
 
     self.clear_items()
     await interaction.response.edit_message(view = self)
     self.value = False
     self.stop()
+
+  async def interaction_check(self, interaction: discord.Interaction):
+    
+    if self.ctx.author.id != interaction.user.id:
+      return await interaction.response.send_message(content = f"You Can't Use that button, {self.authorized_user.mention} is the author of this message.", ephemeral = True)
+
+    return True
 
 class dm_or_ephemeral(discord.ui.View):
   def __init__(self, authorized_user: typing.Union[discord.User, discord.Member] = None, paginator_objects : list = None , **kwargs):
