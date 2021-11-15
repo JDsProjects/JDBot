4f3782cbe45b84403418524494447f011514d0b7
JDJG Inc. Official <ghostzap123@gmail.com>
2021-11-15 07:05:53 +0000

    hmm

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,4 +1,4 @@
-import discord
+import discord, random
 import utils
 from discord.ext import commands
 
@@ -17,7 +17,43 @@ class Economy(commands.Cog):
 
     member = member or ctx.author
 
-    await ctx.send("Being Upgraded from mongodb to sql, please wait")
+    cur = await self.bot.sus_users.cursor()
+    cursor = await cur.execute("SELECT * FROM economy WHERE user_id = (?)", (member.id,))
+    economy = await cursor.fetchone()
+    await cur.close()
+
+    if not economy:
+      view = utils.BasicButtons(ctx)
+      msg = await ctx.send(f"{member} needs to join the database. You can do it now", view = view)
+
+      await view.wait()
+      
+      if view.value is None:
+        return await msg.edit("you didn't respond quickly enough")
+
+      if not view.value:
+        return await msg.edit("Not adding you to the database")
+
+      if view.value:
+        await ctx.send("adding you to the database for economy...")
+
+        cur = await self.bot.sus_users.cursor()
+        await cur.execute("INSERT INTO  VALUES (?,)", (member.id,))
+        await self.bot.sus_users.commit()
+
+        cur = await self.bot.sus_users.cursor()
+        cursor = await cur.execute("SELECT * FROM economy WHERE user_id = (?)", (member.id,))
+
+        economy = await cursor.fetchone()
+        await cur.close()
+            
+
+    data = tuple(economy)
+    wallet = data[1]
+    bank = data[-1]
+
+    embed = discord.Embed(title=f"{member}'s Balance:", description = f"Wallet : {wallet} \nBank : {bank}", color = random.randint(0, 16777215))
+    await ctx.send(embed = embed)
   
 
 def setup(bot):
