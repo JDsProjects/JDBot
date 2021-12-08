a924fd13c3844d3197753f10d41f3e73f915285a
JDJG Inc. Official <ghostzap123@gmail.com>
2021-12-08 22:59:04 +0000

    alright

--- utils/embeds.py
+++ utils/embeds.py
@@ -164,4 +164,5 @@ async def jail_converter(url, ctx):
   embed.set_author(name=f"Jail Image requested by {ctx.author}",icon_url=(ctx.author.display_avatar.url))
   embed.set_image(url = imgur_url["link"])
   embed.set_footer(text="powered by dagpi")
-  await ctx.send(embed = embed)
+  
+  return embed
