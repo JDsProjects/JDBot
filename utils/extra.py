f6fe55698f04246aaf8a34bee80dbf2c489e889c
JDJG Inc. Official <ghostzap123@gmail.com>
2021-12-09 19:54:48 +0000

    hmm

--- utils/extra.py
+++ utils/extra.py
@@ -106,6 +106,7 @@ def npm_create_embed(data : dict):
     for author_data in data["authors"]:
       formatted_author += f"Email: {author_data.get('email', 'Not Provided')}\nName: {author_data['name']}\n\n"
     else:
+      print(data['authors'])
       formatted_author += f"Email: {data['authors']['email']}\n{data['authors']['name']}"
     e.add_field(name = "**Author:**", value = f"```yaml\n{formatted_author}```", inline = False)
     e.add_field(name = "**License:**", value = f"```\n{data.get('license')}```", inline = False)
