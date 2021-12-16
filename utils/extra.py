4a45770d2ea31bd069e16c4aec6fb7dca658ddf0
JDJG Inc. Official <ghostzap123@gmail.com>
2021-12-16 04:02:30 +0000

    Hmmm....

--- utils/extra.py
+++ utils/extra.py
@@ -79,7 +79,7 @@ def cc_generate():
  8107ECA2 {bit_generator()}00""".upper()
 
 async def post(bot, code):
-  async with bot.session.post("https://bin.charles-bot.com/documents", data = code) as resp:
+  async with bot.session.post("https://bin.charles-bot.com/documents", data = code, headers = {"Accept": "application/json"}) as resp:
       data = await resp.json()
       url = f"https://bin.charles-bot.com/{data['key']}"
       return url
