f2d6ca04584a848d95301d1ad18317dd053836c5
JDJG Inc. Official <ghostzap123@gmail.com>
2021-12-27 20:58:29 +0000

    Hmm

--- cogs/help.py
+++ cogs/help.py
@@ -1,47 +1,11 @@
 from discord.ext import commands as Commands
 import utils
-import itertools
 
 class JDBotHelp(Commands.MinimalHelpCommand):
   async def send_pages(self):
     menu = utils.SendHelp(self.paginator.pages, ctx = self.context, delete_message_after = True)
 
-    await menu.send(self.context.channel)
-
-  def add_bot_commands_formatting(self, heading):
-    self.paginator.add_line(f'{heading}')
-
-  async def send_bot_help(self, mapping):
-    ctx = self.context
-    bot = ctx.bot
-
-    if bot.description:
-      self.paginator.add_line(bot.description, empty = True)
-
-    note = self.get_opening_note()
-
-    if note:
-      self.paginator.add_line(note, empty=True)
-
-    no_category = f'\u200b{self.no_category}'
-
-    def get_category(command, *, no_category=no_category):
-      cog = command.cog
-      return f"**{cog.qualified_name}** \n{cog.description}" if cog is not None else no_category
-
-    filtered = await self.filter_commands(bot.commands, sort = True, key=get_category)
-    to_iterate = itertools.groupby(filtered, key=get_category)
-
-    for category, commands in to_iterate:
-      self.add_bot_commands_formatting(category)
-
-    note = self.get_ending_note()
-    if note:
-      self.paginator.add_line()
-      self.paginator.add_line(note)
-
-    await self.send_pages()
-      
+    await menu.send(self.context.channel)  
 
 class Help(Commands.Cog):
   "The Help Menu Cog"
