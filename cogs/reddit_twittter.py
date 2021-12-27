b60c65be01ff00b9fec9885c0c39f43309fe0d5e
JDJG Inc. Official <ghostzap123@gmail.com>
2021-12-27 22:07:45 +0000

    Some Reddit libraries.

--- cogs/reddit_twittter.py
+++ cogs/reddit_twittter.py
@@ -23,7 +23,7 @@ class Reddit(commands.Cog):
 
   @commands.command(brief="Random Meme from Dank Memes with asyncpraw")
   async def dankmeme(self,ctx):
-    embed=await self.asyncpraw_handler("dankmeme")
+    embed = await self.asyncpraw_handler("dankmeme")
     await ctx.send(embed=embed)
 
   @commands.command()
@@ -32,18 +32,89 @@ class Reddit(commands.Cog):
     await ctx.send(embed=embed)
 
   @commands.command()
-  async def codingmemes(self,ctx):
+  async def codingmemes(self, ctx):
     embed = await self.asyncpraw_handler("codingmemes")
     await ctx.send(embed=embed)
 
-  @commands.command(brief="Random meme from Dank Memes with aiohttp",help="Content returned may not be suitable to younger audiences")
-  async def dankmeme2(self,ctx):
-    e = await self.bot.session.get("https://www.reddit.com/r/dankmemes/.json")
-    data = random.choice((await e.json())["data"]["children"])["data"]
-    embed = discord.Embed(title="r/dankmemes",description=f'[{data["title"]}](https://reddit.com{data["permalink"]})', color=0x00FF00)
-    embed.set_image(url=data["url"])
-    embed.set_footer(text=f"Upvote ratio : {data['upvote_ratio']}")
-    await ctx.send(embed=embed)
+  @commands.command()
+  async def smg4(self, ctx):
+    embed = await self.asyncpraw_handler("smg4")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def insaneparents(self, ctx):
+    embed = await self.asyncpraw_handler("insaneparents")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def supermariogalaxy2(self, ctx):
+    embed = await self.asyncpraw_handler("supermariogalaxy2")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def supermariogalaxy(self, ctx):
+    embed = await self.asyncpraw_handler("supermariogalaxy")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def insaneparentsmemes(self, ctx):
+    embed = await self.asyncpraw_handler("insaneparentsmemes")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def entitledparents(self, ctx):
+    embed = await self.asyncpraw_handler("entitledparents")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def dankmemer(self, ctx):
+    embed = await self.asyncpraw_handler("dankmemer")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def memes(self, ctx):
+    embed = await self.asyncpraw_handler("memes")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def askreddit(self, ctx):
+    embed = await self.asyncpraw_handler("askreddit")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def memeeconomy(self, ctx):
+    embed = await self.asyncpraw_handler("memeeconomy")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def dankmemes(self, ctx):
+    embed = await self.asyncpraw_handler("dankmemes")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def funny(self, ctx):
+    embed = await self.asyncpraw_handler("funny")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def ask(self, ctx):
+    embed = await self.asyncpraw_handler("ask")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def gametheorists(self, ctx):
+    embed = await self.asyncpraw_handler("gametheorists")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def advice(self, ctx):
+    embed = await self.asyncpraw_handler("advice")
+    await ctx.send(embed = embed)
+
+  @commands.command()
+  async def discordapp(self, ctx):
+    embed = await self.asyncpraw_handler("discordapp")
+    await ctx.send(embed = embed)
 
 class Twitter(commands.Cog):
   "Commands related to Twitter"
