d36c69cd507d5ab0676f21c47e01fda55aa349d5
JDJG Inc. Official <ghostzap123@gmail.com>
2021-12-29 05:42:32 +0000

    Better Help Lol.

--- cogs/help.py
+++ cogs/help.py
@@ -1,11 +1,43 @@
 from discord.ext import commands
 import utils
+import itertools
 
 class JDBotHelp(commands.MinimalHelpCommand):
   async def send_pages(self):
     menu = utils.SendHelp(self.paginator.pages, ctx = self.context, delete_message_after = True)
 
     await menu.send(self.context.channel)
+
+  async def send_bot_help(self, mapping):
+    ctx = self.context
+    bot = ctx.bot
+
+    if bot.description:
+      self.paginator.add_line(bot.description, empty = True)
+
+    note = self.get_opening_note()
+
+    if note:
+      self.paginator.add_line(note, empty=True)
+
+    no_category = f'\u200b{self.no_category}'
+
+    def get_category(command, *, no_category=no_category):
+      cog = command.cog
+      return f"__**{cog.qualified_name}:**__ \n{cog.description}" if cog is not None else no_category
+
+    filtered = await self.filter_commands(bot.commands, sort = True, key=get_category)
+    to_iterate = itertools.groupby(filtered, key=get_category)
+
+    for category, Commands in to_iterate:
+      self.paginator.add_line(category)
+
+    note = self.get_ending_note()
+    if note:
+      self.paginator.add_line()
+      self.paginator.add_line(note)
+
+    await self.send_pages()
 
 class Help(commands.Cog):
   "The Help Menu Cog"
