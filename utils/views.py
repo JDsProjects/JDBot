80258cf50fccc74fc63e97cfdc84f29fe744aa69
JDJG Inc. Official <ghostzap123@gmail.com>
2022-01-06 01:50:34 +0000

    Hmmm

--- utils/views.py
+++ utils/views.py
@@ -463,25 +463,6 @@ class ScanGlobalEmbed(Paginator):
     embed.set_footer(text = f"Sus Reason : {await get_sus_reason(self.ctx, item)}")
     return embed
 
-async def valid_src(url: str, session: aiohttp.ClientSession):
-  async with session.head(url) as resp:
-    status = resp.status
-  return status == 200
-
-async def emoji_to_url(char, include_check: bool = True, session: aiohttp.ClientSession = None):
-  try:
-    url = f"https://twemoji.maxcdn.com/v/latest/72x72/{ord(char[0]):x}.png"
-    if not include_check:
-      return url
-        
-    _session = session or aiohttp.ClientSession()
-    is_valid = await valid_src(url, _session)
-    if not session:
-      await _session.close()
-    return (url if is_valid else char)
-  except TypeError:
-    return char
-
 class EmojiInfoEmbed(Paginator):
   async def format_page(self, item):
     if isinstance(item, discord.PartialEmoji):
@@ -489,12 +470,13 @@ class EmojiInfoEmbed(Paginator):
         digit = f"{ord(str(item)):x}"
         unicode = f"\\U{digit:>08}"
         emoji_name = item.name.replace(':','')
-        emoji_url = await emoji_to_url(f"{item}", session = self.ctx.bot.session)
+        #emoji_url = await emoji_to_url(f"{item}", session = self.ctx.bot.session)
+        #wip
+        emoji_url = "https://i.imgur.com/3ZUrjUP.png"
         embed=discord.Embed(title="Default Emote:",url=f"http://www.fileformat.info/info/unicode/char/{digit}",color=random.randint(0, 16777215))
         embed.add_field(name = "Name:",value = f"{emoji_name}")
         embed.add_field(name = "Unicode:",value = unicode)
         embed.add_field(name = "unicode url", value = f"[site](http://www.fileformat.info/info/unicode/char/{digit})")
-        embed.add_field(name = "Credit:", value = f"[[Site 1]](https://github.com/Rapptz/RoboDanny/blob/rewrite/cogs/meta.py#L250-L264)")
         embed.set_image(url = emoji_url)
         embed.set_footer(text = f"click the title for more unicode data")
         return embed
