f10938e3840bb5b35c071390641da3108a857299
dartmern <eric.ni.cheng@gmail.com>
2022-01-09 19:46:49 -0800

    first

--- cogs/webhook.py
+++ cogs/webhook.py
@@ -4,6 +4,8 @@ import re
 import random
 import aiohttp
 
+from JDBot.utils.converters import WebhookConverter
+
 
 class Webhook(commands.Cog):
     "Commands dealing with webhooks"
@@ -12,113 +14,80 @@ class Webhook(commands.Cog):
         self.bot = bot
 
     @commands.command(brief="a way to send stuff to webhooks.", help="this uses webhook urls, and sends stuff to them")
-    async def webhook(self, ctx, *, args=None):
-        if args is None:
+    async def webhook(self, ctx: commands.Context, webhook: WebhookConverter, *, content: str = None):
+        if not content:
             await ctx.send("You didn't send anything")
 
-        if args:
-            check = re.match(
-                r"https://discord(?:app)?.com/api/webhooks/(?P<id>[0-9]{17,21})/(?P<token>[A-Za-z0-9\.\-\_]{60,68})",
-                args,
+        session = self.bot.session
+        response = await session.get(webhook.group())
+
+        if response.status != 200:
+            return await ctx.send("Not a valid link or an error occured.")
+        
+        if response.status == 200:
+            webhook = discord.Webhook.from_url(webhook.group(), session=session)
+
+            embed = discord.Embed(
+                    title=f"Webhook {webhook.name}'s Message",
+                    color=random.randint(0, 16777215),
+                    timestamp=(ctx.message.created_at),
             )
+            embed.add_field(name="Content:", value=content)
+            await webhook.send(embed=embed)
 
-            if check:
-                args = args.replace(f"{check.group()} ", "")
-                if args == check.group():
-                    args = "No Content"
-
-                    session = self.bot.session
-                    response = await session.get(check.group())
-                    if response.status == 200:
-                        webhook = discord.Webhook.from_url(check.group(), session=session)
-
-                        embed = discord.Embed(
-                            title=f"Webhook {webhook.name}'s Message",
-                            color=random.randint(0, 16777215),
-                            timestamp=(ctx.message.created_at),
-                        )
-                        embed.add_field(name="Content:", value=args)
-                        await webhook.send(embed=embed)
-
-                        await ctx.send(f"Message was sent to the desired webhook channel.")
-
-                    if response.status != 200:
-                        await ctx.send("Not a valid link or an error occured")
-
-                if isinstance(ctx.channel, discord.TextChannel):
-                    try:
-                        await ctx.message.delete()
-                    except:
-                        await ctx.send("deleting the webhook failed, delete asap")
-
-            if not check:
-                await ctx.send("not a proper webhook url.")
+            await ctx.send(f"Message was sent to the desired webhook channel.")
 
     @commands.command(brief="a way to create webhooks", help="make commands with this.")
-    async def webhook_create(self, ctx, arg=None, *, args=None):
-        if isinstance(ctx.channel, discord.TextChannel):
+    async def webhook_create(self, ctx, name: str=None, *, args=None):
+        if ctx.guild:
             if ctx.author.guild_permissions.manage_webhooks:
-                if arg:
-                    if args is None:
-                        try:
-                            webhook = await ctx.channel.create_webhook(name=arg)
-                        except Exception as e:
-                            return await ctx.send(
+                if not name:
+                    return await ctx.send("Please input a webhook name.")
+                
+                try:
+                    webhook = await ctx.channel.create_webhook(name=name)
+                except Exception as e:
+                    return await ctx.send(
                                 f"give the bot manage webhook permissions for this to work and give the error to {e} if an issue."
-                            )
-                        embed = discord.Embed(
-                            title=f"{ctx.author}'s message:",
-                            color=random.randint(0, 16777215),
-                            timestamp=(ctx.message.created_at),
                         )
-                        embed.add_field(name="Content:", value="Test")
-                    if args:
-                        try:
-                            webhook = await ctx.channel.create_webhook(name=arg, reason=args)
+                embed = discord.Embed(
+                    title=f"{ctx.author}'s message:",
+                    color=random.randint(0, 16777215),
+                    timestamp=(ctx.message.created_at),
+                )
+                embed.add_field(name="Content:", value=args or "Test")
 
-                        except Exception as e:
-                            return await ctx.send(
-                                f"give the bot manage webhook permissions for this to work and give the error to {e} if an issue."
-                            )
-
-                        embed = discord.Embed(
-                            title=f"{ctx.author}'s message:",
-                            color=random.randint(0, 16777215),
-                            timestamp=(ctx.message.created_at),
-                        )
-                        embed.add_field(name="Content:", value=args)
-
-                    if ctx.message.attachments:
-                        image = await ctx.message.attachments[0].read()
-                        pass_test = True
-                        try:
-                            discord.utils._get_mime_type_for_image(image)
-                        except discord.errors.InvalidArgument:
-                            pass_test = False
-
-                        if pass_test:
-                            await webhook.edit(avatar=image)
-                        if pass_test is False:
-                            await ctx.send("not a valid image")
-
-                    await webhook.send(embed=embed)
-
-                    if ctx.author.dm_channel is None:
-                        await ctx.author.create_dm()
-
+                if ctx.message.attachments:
+                    await ctx.trigger_typing()
+                    
+                    image = await ctx.message.attachments[0].read()
+                    pass_test = True
                     try:
-                        await ctx.author.send("Webhook url coming up")
-                        await ctx.author.send(webhook.url)
-                    except discord.Forbidden:
-                        await ctx.send(f"We couldn't DM you {ctx.author.mention}")
+                        discord.utils._get_mime_type_for_image(image)
+                    except discord.errors.InvalidArgument:
+                        pass_test = False
 
-                if arg is None:
-                    await ctx.send("You need to use values for it to work")
-            if ctx.author.guild_permissions.manage_webhooks is False:
-                await ctx.send("you can't use that.")
+                    if pass_test:
+                        await webhook.edit(avatar=image)
+                    if pass_test is False:
+                        await ctx.send("not a valid image")
 
-        if isinstance(ctx.channel, discord.DMChannel):
-            await ctx.send("You can't use that silly")
+                await webhook.send(embed=embed)
+
+                if ctx.author.dm_channel is None:
+                    await ctx.author.create_dm()
+
+                try:
+                    await ctx.author.send("Webhook url coming up")
+                    await ctx.author.send(webhook.url)
+                except discord.Forbidden:
+                    await ctx.send(f"We couldn't DM you {ctx.author.mention}")
+
+            else:
+                return await ctx.send("You don't have sufficient permissions.")
+            
+        else:
+            return await ctx.send("You cannot use this in DMs!")
 
     @commands.command(brief="tells you a webhook's avatar.")
     async def webhook_avatar(self, ctx, *, args=None):
