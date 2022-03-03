096e07396324afac3e7b3b869311c2cb3baec925
JDJG Inc. Official <ghostzap123@gmail.com>
2022-03-03 18:53:53 -0500

    fixed

--- utils/embeds.py
+++ utils/embeds.py
@@ -215,3 +215,5 @@ async def jail_converter(url, ctx):
     embed.set_author(name=f"Jail Image requested by {ctx.author}", icon_url=(ctx.author.display_avatar.url))
     embed.set_image(url=url)
     embed.set_footer(text="powered by dagpi")
+
+    return embed
