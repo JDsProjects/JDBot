ee7b5389070c5c88dc9e251bb3e330fcea0e2fdb
JDJG Inc. Official <ghostzap123@gmail.com>
2022-03-06 11:50:57 -0500

    there

--- utils/views.py
+++ utils/views.py
@@ -1324,7 +1324,6 @@ class ChatBotModal(discord.ui.Modal):
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.defer()
-        await interaction.followup.send(content="Message Received.", ephemeral=True)
         arg = self.children[0].value
         self.view.message = await interaction.followup.send(f"{arg}", view=self.view, ephemeral=True)
 
