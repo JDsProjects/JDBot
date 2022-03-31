f8f69d8ffcec7a3ac08d1eef9d952e7da506e141
JDJG Inc. Official <ghostzap123@gmail.com>
2022-03-30 23:43:01 -0400

    okay there

--- cogs/order.py
+++ cogs/order.py
@@ -319,6 +319,7 @@ class Order(commands.Cog):
 
         embed.set_author(name=f"order for {ctx.author}:", icon_url=ctx.author.display_avatar.url)
 
+        embed.add_field(name="Time Spent:", value=f"{int((time_after - time_before)*1000)}MS")
         embed.add_field(name="Powered by:", value="GIPHY")
         embed.set_image(url=f"https://media3.giphy.com/media/{gifNearest.id}/giphy.gif")
 
@@ -377,7 +378,6 @@ class Order(commands.Cog):
 
         embed.set_author(name=f"Random Disco Gif for {ctx.author}:", icon_url=ctx.author.display_avatar.url)
 
-        embed.add_field(name="Time Spent:", value=f"{int((time_after - time_before)*1000)}MS")
         embed.add_field(name="Powered by:", value="Tenor")
 
         if gifNearest.gif:
