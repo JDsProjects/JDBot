4bd2ef26192926d24a28a590367352d8d92dc650
JDJG Inc. Official <ghostzap123@gmail.com>
2022-04-08 20:16:26 -0400

    test time

--- cogs/info.py
+++ cogs/info.py
@@ -101,25 +101,6 @@ class Info(commands.Cog):
             inline=False,
         )
 
-        join_badges: str = "\u0020".join(badges) if badges else "N/A"
-        join_statuses = (
-            " \n| ".join(f"**{name}**: {value}" for name, value in statuses) if statuses else "**Status**: \nUnknown"
-        )
-
-        embed.add_field(
-            name="User Info 2:",
-            value=f"Type: {user_type} \nBadges: {join_badges} \n**Joined Discord**: {discord.utils.format_dt(user.created_at, style = 'd')}\n{discord.utils.format_dt(user.created_at, style = 'T')}\n {join_statuses}",
-            inline=False,
-        )
-
-        embed.add_field(
-            name="Guild Info:",
-            value=f"**Joined Guild**: {joined_guild} \n**Nickname**: {nickname} \n**Highest Role:** {highest_role}",
-            inline=False,
-        )
-
-        embed.set_image(url=user.display_avatar.url)
-
         guilds_list = utils.grab_mutualguilds(ctx, user)
 
         pag = commands.Paginator(prefix="", suffix="")
@@ -135,9 +116,38 @@ class Info(commands.Cog):
         menu = utils.MutualGuildsEmbed(pages, ctx=ctx, disable_after=True)
         view = utils.UserInfoSuper(ctx, menu, ctx.author.dm_channel)
 
+        view.data.basic_info = embed
+
+        embed.remove_field(0)
+
+        join_badges: str = "\u0020".join(badges) if badges else "N/A"
+        join_statuses = (
+            " \n| ".join(f"**{name}**: {value}" for name, value in statuses) if statuses else "**Status**: \nUnknown"
+        )
+
+        embed.add_field(
+            name="User Info 2:",
+            value=f"Type: {user_type} \nBadges: {join_badges} \n**Joined Discord**: {discord.utils.format_dt(user.created_at, style = 'd')}\n{discord.utils.format_dt(user.created_at, style = 'T')}\n {join_statuses}",
+            inline=False,
+        )
+
+        view.data.basic_info2 = embed
+
+        embed.remove_field(0)
+
+        embed.add_field(
+            name="Guild Info:",
+            value=f"**Joined Guild**: {joined_guild} \n**Nickname**: {nickname} \n**Highest Role:** {highest_role}",
+            inline=False,
+        )
+
+        view.data.guild_info = embed
+        embed.remove_field(0)
+
+        embed.set_image(url=user.display_avatar.url)
+
         await ctx.send(
             "Pick a way for Mutual Guilds to be sent to you or not if you really don't the mutualguilds",
-            embed=embed,
             view=view,
         )
 
