2636fcfc3020793b846a91582d02879404bee601
JDJG Inc. Official <ghostzap123@gmail.com>
2022-04-25 20:46:43 -0400

    Let's see

--- cogs/info.py
+++ cogs/info.py
@@ -88,6 +88,43 @@ class Info(commands.Cog):
                 title=f"{user}", color=random.randint(0, 16777215), timestamp=self.ctx.message.created_at
             )
 
+            statuses = []
+
+            if isinstance(user, discord.Member):
+                nickname = user.nick
+                joined_guild = f"{discord.utils.format_dt(user.joined_at, style = 'd')}\n{discord.utils.format_dt(user.joined_at, style = 'T')}"
+                highest_role = user.top_role
+
+                for name, status in (
+                    ("Status", user.status),
+                    ("Desktop", user.desktop_status),
+                    ("Mobile", user.mobile_status),
+                    ("Web", user.web_status),
+                ):
+                    statuses.append((name, utils.profile_converter(name.lower(), status)))
+
+            else:
+
+                nickname = "None Found"
+                joined_guild = "N/A"
+                highest_role = "None Found"
+
+                member = discord.utils.find(lambda member: member.id == user.id, self.bot.get_all_members())
+                if member:
+                    for name, status in (
+                        ("Status", member.status),
+                        ("Desktop", member.desktop_status),
+                        ("Mobile", member.mobile_status),
+                        ("Web", member.web_status),
+                    ):
+                        statuses.append((name, utils.profile_converter(name.lower(), status)))
+
+            join_statuses = (
+                " \n| ".join(f"**{name}**: {value}" for name, value in statuses)
+                if statuses
+                else "**Status**: \nUnknown"
+            )
+
             if choice == "basic":
                 embed.add_field(
                     name="User Info: ",
@@ -128,13 +165,10 @@ class Info(commands.Cog):
                 embed.set_image(url=user.display_avatar.url)
                 embed.set_footer(text=f"Requested by {self.ctx.author}")
 
-            if choice == "status" and 2 == 3:
-                embed.add_field(name=f"{self.view.join_statuses}", value="\u2800", inline=False)
+            if choice == "status":
+                embed.add_field(name=f"{join_statuses}", value="\u2800", inline=False)
 
-            if choice == "guildinfo" and 3 == 1:
-                joined_guild = self.view.joined_guild
-                nickname = self.view.nickname
-                highest_role = self.view.highest_role
+            if choice == "guildinfo":
                 embed.add_field(
                     name="Guild Info:",
                     value=f"**Joined Guild**: {joined_guild} \n**Nickname**: {nickname} \n**Highest Role:** {highest_role}",
@@ -151,62 +185,8 @@ class Info(commands.Cog):
     async def userinfo(self, ctx, *, user: utils.BetterUserconverter = None):
 
         user = user or ctx.author
-        user_type = "Bot" if user.bot else "User" if isinstance(user, discord.User) else "Member"
-
-        statuses = []
-
-        if isinstance(user, discord.Member):
-            nickname = user.nick
-            joined_guild = f"{discord.utils.format_dt(user.joined_at, style = 'd')}\n{discord.utils.format_dt(user.joined_at, style = 'T')}"
-            highest_role = user.top_role
-
-            for name, status in (
-                ("Status", user.status),
-                ("Desktop", user.desktop_status),
-                ("Mobile", user.mobile_status),
-                ("Web", user.web_status),
-            ):
-                statuses.append((name, utils.profile_converter(name.lower(), status)))
-
-        else:
-
-            nickname = "None Found"
-            joined_guild = "N/A"
-            highest_role = "None Found"
-
-            member = discord.utils.find(lambda member: member.id == user.id, self.bot.get_all_members())
-            if member:
-                for name, status in (
-                    ("Status", member.status),
-                    ("Desktop", member.desktop_status),
-                    ("Mobile", member.mobile_status),
-                    ("Web", member.web_status),
-                ):
-                    statuses.append((name, utils.profile_converter(name.lower(), status)))
 
         embed = discord.Embed(title=f"{user}", color=random.randint(0, 16777215), timestamp=ctx.message.created_at)
-
-        embed.add_field(
-            name="User Info: ",
-            value=f"**Username**: {user.name} \n**Discriminator**: {user.discriminator} \n**ID**: {user.id}",
-            inline=False,
-        )
-
-        join_statuses = (
-            " \n| ".join(f"**{name}**: {value}" for name, value in statuses) if statuses else "**Status**: \nUnknown"
-        )
-
-        embed.add_field(
-            name="User Info 2:",
-            value=f"\n**Joined Discord**: {discord.utils.format_dt(user.created_at, style = 'd')}\n{discord.utils.format_dt(user.created_at, style = 'T')}\n {join_statuses}",
-            inline=False,
-        )
-
-        embed.add_field(
-            name="Guild Info:",
-            value=f"**Joined Guild**: {joined_guild} \n**Nickname**: {nickname} \n**Highest Role:** {highest_role}",
-            inline=False,
-        )
 
         embed.set_image(url=user.display_avatar.url)
 
