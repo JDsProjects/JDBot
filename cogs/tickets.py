c18212c00fa905d77cb7f1dfbf691845c8467a09
Zeno <57388886+ZenoABC@users.noreply.github.com>
2022-05-07 21:34:57 +0300

    Update tickets.py

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -98,19 +98,16 @@ class Ticket(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        print("Starting.")
+        print(message.channel)
         if message.guild.id == 736422329399246990 and message.channel.id in self.ticket_cache:
-            print("Guild ID and mssage channel id matches fine.")
             author = self.ticket_cache[message.channel.id]["author"]
             author = self.bot.get_user(author)
             await author.send(f"`{message.author}:` {message.content}")
-            print("Sent.")
         elif not message.guild and message.author.id in self.ticket_cache:
             print("Second condition matches fine.")
             thread = self.ticket_cache[message.author.id]["remote"]
             thread = self.bot.get_channel(thread)
             await thread.send(f"`{message.author}:` {message.content}")
-            print("Sent.")
 
 async def setup(bot: JDBot):
     await bot.add_cog(Ticket(bot))
