219f5df8ee8bfe8601c3156aa8abd99a16e33071
Zeno <57388886+ZenoABC@users.noreply.github.com>
2022-05-07 21:40:08 +0300

    Update tickets.py (#62)

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -98,13 +98,14 @@ class Ticket(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        if message.author.bot: 
+            return
         print(message.channel)
         if message.guild and message.guild.id == 736422329399246990 and message.channel.id in self.ticket_cache:
             author = self.ticket_cache[message.channel.id]["author"]
             author = self.bot.get_user(author)
             await author.send(f"`{message.author}:` {message.content}")
         elif not message.guild and message.author.id in self.ticket_cache:
-            print("Second condition matches fine.")
             thread = self.ticket_cache[message.author.id]["remote"]
             thread = self.bot.get_channel(thread)
             await thread.send(f"`{message.author}:` {message.content}")
