705e871dffb81240e093eb517a50ed7d456b355e
JDJG Inc. Official <ghostzap123@gmail.com>
2022-05-07 18:22:20 -0400

    it's back boiis

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -122,6 +122,17 @@ class Ticket(commands.Cog):
         if message.author.bot:
             return
 
+        context = await self.bot.get_context(message)
+
+        if isinstance(message.channel, discord.DMChannel):
+            if context.prefix is None or self.bot.user.mentioned_in(message):
+                if message.author.id != self.bot.user.id and context.valid is False:
+                    await message.channel.send(
+                        "Ticket Support is coming soon. For now Contact our Developers: Shadi#9492 or JDJG Inc. Official#3493"
+                    )
+
+        # edit later idk
+
         if not self.main_channel:
             self.main_channel = self.bot.get_channel(855947100730949683)
             self.support_role = self.bot.get_guild(736422329399246990).get_role(855219483295875072)
