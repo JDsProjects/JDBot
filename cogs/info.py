9a357d743db224f58eed21fd830571bbc1663bc3
JDJG Inc. Official <ghostzap123@gmail.com>
2022-08-08 13:08:19 -0400

    Hmmm

--- cogs/info.py
+++ cogs/info.py
@@ -361,6 +361,8 @@ class DevTools(commands.Cog):
 
             else:
                 results = [r for r in unfiltered_results if r in results]
+
+                print(results)
                 return results
 
     async def rtfm_send(self, ctx, results):
