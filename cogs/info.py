8c9e34b7648e93168257fd8b44b22239548d9fe1
JDJG Inc. Official <ghostzap123@gmail.com>
2022-08-07 23:04:21 -0400

    Still is being worked on

--- cogs/info.py
+++ cogs/info.py
@@ -350,7 +350,9 @@ class DevTools(commands.Cog):
             # results = await res.json()
 
             unfiltered_results = await utils.rtfm(self.bot, url)
+
             results = get_close_matches(args, unfiltered_results, n=10, cutoff=0.6)
+            # this still needs to be fixed.
 
             if not results:
                 return f"Could not find anything with {args}."
