2704cd419035b1f2232105707eb29bf961a85f53
JDJG Inc. Official <ghostzap123@gmail.com>
2022-09-02 13:56:21 -0400

    More have not been listed yet, but I will make sure they are

--- cogs/Bot.py
+++ cogs/Bot.py
@@ -699,6 +699,8 @@ class Bot(commands.Cog):
             150665783268212746,
             351656397257310212,
             746807014658801704,
+            910899642236043294,
+            343019667511574528,
         ]
         users = sorted([(f"{await self.bot.try_user(uid)}") or f"Unknown User#0000 ({uid})" for uid in to_credit])
         joined_users = "\n".join(users)
