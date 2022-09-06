a1af1007982e3e991d43e51caf2001b52b2a9c69
JDJG Inc. Official <ghostzap123@gmail.com>
2022-09-06 13:57:38 -0400

    Adds what soheab suggested, I think?

--- cogs/test.py
+++ cogs/test.py
@@ -79,7 +79,7 @@ class Test(commands.Cog):
         username_id = username.data.id
         username_url = username.data.url
 
-        # maybe also grab the profile_image_url ?
+        image = username.profile_image_url
 
         try:
             await ctx.send("work in progress")
@@ -108,7 +108,13 @@ class Test(commands.Cog):
 
             tweet_url = f"https://twitter.com/twitter/statuses/{tweet.id}"
 
-            embed = discord.Embed(title=f"Users Requested {amount} tweet)", description=f"{tweet.text}")
+            embed = discord.Embed(title=f"Tweet!", description=f"{tweet.text}", url=tweet_url, color=0x1DA1F2)
+            embed.set_author(icon_url=image)
+            embed.set_footer(text=f"Requested by {ctx.author}\nJDJG does not own any of the content of the tweets")
+            embed.set_thumbnail(url="https://i.imgur.com/zpLkfHo.png")
+
+            # I don't know how to manage the twitter attachments, it may be nsfwish as well
+
             embeds.append(embed)
 
         await ctx.send(embeds=embeds[0:4])
