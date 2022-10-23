c41975d5028d428553053072befe1a28a7be0bdd
JDJG Inc. Official <ghostzap123@gmail.com>
2022-10-23 12:32:30 -0400

    Let's see

--- utils/views.py
+++ utils/views.py
@@ -447,6 +447,30 @@ class Paginator(View):
 # thank you so much Soheab for allowing me to use this paginator you made and putting in the work to do this :D (That's his github name so...)
 
 
+class EmojiInfoEmbed(Paginator):
+    async def format_page(self, item):
+
+        embed = discord.Embedcolor = random.randint(0, 16777215)
+        embed.set_image(url=item.url)
+        # only if valid emoji
+
+        if item.is_unicode:
+
+            embed.title = "Default Emote:"
+            # embed.url = f"http://www.fileformat.info/info/unicode/char/{digit}"
+
+            # embed.add_field(name="Name:", value=f"{emoji_name}")
+            # embed.add_field(name="Unicode:", value=unicode)
+            # embed.add_field(        name="unicode url", value=f"[site](http://www.fileformat.info/info/unicode/char/{digit})")
+            # embed.set_footer(text=f"click the title for more unicode data")
+
+        else:
+            embed.title = f"Custom Emoji: **{item.name}**"
+            embed.set_footer(text=f"Emoji ID:{item.id}")
+
+        return embed
+
+
 class MutualGuildsEmbed(Paginator):
     def format_page(self, item):
         embed = discord.Embed(title="Mutual Servers:", description=item, color=random.randint(0, 16777215))
@@ -553,10 +577,12 @@ class InviteInfoEmbed(Paginator):
                 image = item.guild.icon.url if item.guild.icon else "https://i.imgur.com/3ZUrjUP.png"
                 guild = item.guild
                 guild_id = item.guild.id
+
             if item.guild is None:
                 guild = "Group Chat"
                 image = "https://i.imgur.com/pQS3jkI.png"
                 guild_id = "Unknown"
+
             embed = discord.Embed(title=f"Invite for {guild}:", color=random.randint(0, 16777215))
             embed.set_author(name="Discord Invite Details:", icon_url=(image))
             embed.add_field(name="Inviter:", value=f"{item.inviter}")
@@ -633,45 +659,6 @@ class ScanGlobalEmbed(Paginator):
         embed.add_field(name="Shared Guilds:", value=f"{guild_join(grab_mutualguilds(self.ctx, item))}")
         embed.set_footer(text=f"Sus Reason : {await get_sus_reason(self.ctx, item)}")
         return embed
-
-
-class EmojiInfoEmbed(Paginator):
-    async def format_page(self, item):
-        if isinstance(item, discord.PartialEmoji):
-            if item.is_unicode_emoji():
-                digit = f"{ord(str(item)):x}"
-                unicode = f"\\U{digit:>08}"
-                emoji_name = item.name.replace(":", "")
-                # emoji_url = await emoji_to_url(f"{item}", session = self.ctx.bot.session)
-                # wip
-                emoji_url = "https://i.imgur.com/3ZUrjUP.png"
-                embed = discord.Embed(
-                    title="Default Emote:",
-                    url=f"http://www.fileformat.info/info/unicode/char/{digit}",
-                    color=random.randint(0, 16777215),
-                )
-                embed.add_field(name="Name:", value=f"{emoji_name}")
-                embed.add_field(name="Unicode:", value=unicode)
-                embed.add_field(
-                    name="unicode url", value=f"[site](http://www.fileformat.info/info/unicode/char/{digit})"
-                )
-                embed.set_image(url=emoji_url)
-                embed.set_footer(text=f"click the title for more unicode data")
-                return embed
-
-            else:
-                embed = discord.Embed(title=f"Custom Emoji: **{item.name}**", color=random.randint(0, 16777215))
-                embed.set_image(url=item.url)
-                embed.set_footer(text=f"Emoji ID:{item.id}")
-                return embed
-
-        else:
-            embed = discord.Embed(
-                title="Failed grabbing emoji:",
-                description=f"Discord couldn't fetch the emoji with regex: {item}",
-                color=random.randint(0, 16777215),
-            )
-            return embed
 
 
 class TodoEmbed(Paginator):
