b44668c2b9d2dea2e46919c805cad37c3192ddaf
Soheab_ <33902984+Soheab@users.noreply.github.com>
2022-10-23 19:51:13 +0200

    Improve emoji info command. (#83)
    * Push everything
    
    * Add different emoji styles to embed

--- utils/emoji.py
+++ utils/emoji.py
@@ -1,18 +1,20 @@
 """ CREDITS: https://github.com/jay3332/pilmoji"""
 
 from __future__ import annotations
+from typing import TYPE_CHECKING, Any, ClassVar, Dict, Final, List, Literal, Optional, Pattern, Tuple, Union
 
-from re import compile as re_compile
-from re import escape as re_escape
-from typing import TYPE_CHECKING, Annotated, Any, ClassVar, Dict, Final, List, Literal, Optional, Pattern
+from re import compile as re_compile, escape as re_escape
 from urllib.parse import quote_plus
 
-from discord import Intents, PartialEmoji
-from discord.ext import commands
 from emoji import unicode_codes
+
+from discord import PartialEmoji
+from discord.ext.commands import Converter, BadArgument, Context
 
 if TYPE_CHECKING:
     from typing_extensions import Self
+
+    from discord.ext.commands import Context
 
     ValidStyles = Literal[
         "apple",
@@ -31,11 +33,22 @@ if TYPE_CHECKING:
         "mozilla",
     ]
 
+__all__: Tuple[str, ...] = ("CustomEmoji", "EmojiConverter", "InvalidEmojis")
+
 language_pack: Dict[str, str] = unicode_codes.get_emoji_unicode_dict("en")  # type: ignore
 _UNICODE_EMOJI_REGEX = "|".join(map(re_escape, sorted(language_pack.values(), key=len, reverse=True)))
 _DISCORD_EMOJI_REGEX = "<a?:[a-zA-Z0-9_]{2,32}:[0-9]{17,22}>"
 
 EMOJI_REGEX: Final[Pattern[str]] = re_compile(f"({_UNICODE_EMOJI_REGEX}|{_DISCORD_EMOJI_REGEX})")
+
+
+class InvalidEmojis(BadArgument):
+    """Raised when a string contains no valid emojis."""
+
+    def __init__(self, full_input: str, splitted_text: List[str]) -> None:
+        self.argument: str = full_input
+        self.splitted_text: List[str] = splitted_text
+        super().__init__(f"Invalid emojis: {self.splitted_text}")
 
 
 class CustomEmoji(PartialEmoji):
@@ -50,21 +63,21 @@ class CustomEmoji(PartialEmoji):
         emoji: Optional[str] = None,
         id: Optional[int] = None,
         style: Optional[ValidStyles] = None,
-        is_unicode: bool = False,
+        unicode: Optional[str] = None,
     ) -> None:
         super().__init__(name=name, animated=animated, id=id)
         self.emoji: Optional[str] = emoji
         self._style: Optional[ValidStyles] = style
-        self.is_unicode: bool = is_unicode
+        self.unicode: Optional[str] = unicode
 
     def is_unicode_emoji(self) -> bool:
-        if self.is_unicode:
+        if self.unicode:
             return False
         return super().is_unicode_emoji()
 
     @property
     def url(self) -> str:
-        if self.is_unicode:
+        if self.unicode:
             return f"{self.EMOJI_URL}{quote_plus(self.emoji)}" + (f"?style={self._style}" if self._style else "")  # type: ignore
         return super().url
 
@@ -72,16 +85,18 @@ class CustomEmoji(PartialEmoji):
     def as_unicode(cls, item: str, style: Optional[ValidStyles] = None) -> Self:
         digit = f"{ord(str(item)):x}"
         unicode = f"\\U{digit:>08}"
-        emoji_name = item.replace(":", "")
-        print("as uni", item, style, digit, unicode, emoji_name)
-        return cls(emoji=item, name=unicode, id=digit, animated=False, style=style, is_unicode=True)  # type: ignore
+        name = item.replace(":", "")
+        return cls(emoji=item, name=name, id=digit, animated=False, style=style, unicode=unicode)  # type: ignore
 
     @classmethod
     def as_emoji(cls, emoji: str) -> Self:
         obj = cls.from_str(str(emoji))
         return cls(emoji=str(obj), name=obj.name, id=obj.id, animated=obj.animated)
 
-    def with_style(self, style: ValidStyles) -> Optional[Self]:
+    def with_style(self, style: ValidStyles) -> Self:
+        if not self.unicode:
+            raise TypeError("Cannot set style for non-unicode emoji")
+
         return self.__class__(name=self.name, animated=self.animated, id=self.id, emoji=self.emoji, style=style)
 
     async def is_valid(self) -> bool:
@@ -92,44 +107,56 @@ class CustomEmoji(PartialEmoji):
         else:
             return True
 
-    async def read(self) -> bytes:
-        print("url used:", self.url, self.emoji)
-        return await super().read()
 
+class EmojiConverter(Converter[Any]):
+    class CovertedEmojis:
+        def __init__(self, input: str, invalid_emojis: List[str], valid_emojis: List[CustomEmoji]) -> None:
+            self.input: str = input
+            self.invalid_emojis: List[str] = invalid_emojis
+            self.valid_emojis: List[CustomEmoji] = valid_emojis
 
-class EmojiConverter(commands.Converter[Any]):
-    def _parse_line(self, line: str, /) -> List[CustomEmoji]:
+        @property
+        def all(self) -> List[Union[str, CustomEmoji]]:
+            return self.valid_emojis + self.invalid_emojis
+
+    def _parse_line(self, line: str, /) -> List[Union[str, CustomEmoji]]:
         emojis = []
         for i, chunk in enumerate(EMOJI_REGEX.split(line)):
             if not chunk:
                 continue
 
             if not i % 2:
-                continue
+                emoji = chunk
 
             if len(chunk) > 18:  # This is guaranteed to be a Discord emoji
-                node = CustomEmoji.as_emoji(chunk)
+                emoji = CustomEmoji.as_emoji(chunk)
             else:
-                node = CustomEmoji.as_unicode(chunk)
+                emoji = CustomEmoji.as_unicode(chunk)
 
-            emojis.append(node)
+            emojis.append(emoji)
 
         return emojis
 
-    def parse_emojis(self, text: str, /) -> List[List[CustomEmoji]]:
+    def parse_emojis(self, text: str, /) -> List[List[Union[str, CustomEmoji]]]:
         return [self._parse_line(line) for line in text.splitlines()]
 
-    async def convert(self, ctx: commands.Context, argument: str):
+    async def convert(self, ctx: Context, argument: str):
         valid_emojis = []
-        emojis: List[List[CustomEmoji]] = self.parse_emojis(argument.strip())
+        invalid_emojis = []
+        emojis: List[List[Union[str, CustomEmoji]]] = self.parse_emojis(argument.strip())
         for line in emojis:
             for emoji in line:
+                if not isinstance(emoji, CustomEmoji):
+                    invalid_emojis.append(emoji)
+                    continue
+
                 emoji._state = ctx.bot._connection  # type: ignore
-                if result := await emoji.is_valid():
+                if await emoji.is_valid():
                     valid_emojis.append(emoji)
+                else:
+                    invalid_emojis.append(str(emoji))
 
-        return valid_emojis
+        if not valid_emojis:
+            raise InvalidEmojis(argument, invalid_emojis)
 
-
-# async def emoji(ctx: commands.Context, *, emojis: Annotated[List[CustomEmoji], EmojiConverter]):
-# await ctx.send(f"{ctx.message.content}\n{emojis=}\n{[e.url for e in emojis]=}")
+        return self.CovertedEmojis(argument, invalid_emojis, valid_emojis)
