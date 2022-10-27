595689263a7265c244312155692e3bec3b2bbf08
JDJG Inc. Official <ghostzap123@gmail.com>
2022-10-26 23:35:06 -0400

    Alr

--- utils/image.py
+++ utils/image.py
@@ -31,10 +31,11 @@ def gadget(text) -> BytesIO:
 
     with Image.new("RGBA", (600, 800), "white") as canv:
         with Image.open("assets/images/gadget.png") as image:
+
             resized = image.resize((600, 600))
             canv.paste(resized, (0, 200))
 
-            draw = ImageDraw.Draw(image)
+            draw = ImageDraw.Draw(canv)
             draw.text((5, 5), text, font=font, fill="black")
 
             canv.save(f, "PNG")
