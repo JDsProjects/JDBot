bd5d635300621f3d7169fb1e3c43cf92babc503e
JDJG Inc. Official <ghostzap123@gmail.com>
2022-10-26 22:59:09 -0400

    Thanks Jeyy so far

--- utils/image.py
+++ utils/image.py
@@ -22,3 +22,17 @@ def call_text(text) -> BytesIO:
 
     f.seek(0)
     return f
+
+
+def gadget(text) -> BytesIO:
+
+    f = BytesIO()
+
+    with Image.new("RGBA", (600, 800), "white") as canv:
+        with Image.open("assets/images/gadget.png") as image:
+            resized = image.resize((600, 600))
+            canv.paste(resized, (0, 200))
+            canv.save(f, "PNG")
+
+    f.seek(0)
+    return f
