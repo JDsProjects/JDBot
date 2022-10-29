9b0705b6dc52c58e4b4f14470dec7c48f1d0bbd9
JDJG Inc. Official <ghostzap123@gmail.com>
2022-10-29 14:41:27 -0400

    Fixed

--- utils/views.py
+++ utils/views.py
@@ -507,7 +507,7 @@ class MutualGuildsEmbed(Paginator):
 class cdnViewer(Paginator):
     def format_page(self, item):
         embed = discord.Embed(title="CDN Viewer", description=f"Image ID: {item}", color=random.randint(0, 16777215))
-        embed.set_image(f"https://cdn.jdjgbot.com/image/{item}.gif?opengraph_pass=true")
+        embed.set_image(url=f"https://cdn.jdjgbot.com/image/{item}.gif?opengraph_pass=true")
 
         return embed
 
