a29511f6c487b6c2e6490fe5edcead97c26b59a4
Soheab_ <33902984+Soheab@users.noreply.github.com>
2022-11-08 15:14:36 +0100

    Fix some mistakes in TweetWrapper (#89)

--- utils/tweet.py
+++ utils/tweet.py
@@ -21,7 +21,7 @@ class TweetWrapper:
     def __new__(cls, original_response: TweepyResponse):
         tweets = cls._unwrap_tweets_media(data=original_response.data, includes=original_response.includes)
         return Response(
-            data=tweets,
+            data=original_response.data,
             meta=original_response.meta,
             includes=original_response.includes,
             errors=original_response.errors,
@@ -41,13 +41,14 @@ class TweetWrapper:
 
         media = includes["media"]
         if not media:
-            return [__construct_custom_tweet(x) for x in data.tweets]
+            return [__construct_custom_tweet(x) for x in data]
 
         for tweet in data:
             if tweet.attachments:
                 media_keys = tweet.attachments["media_keys"]
                 if not media_keys:
                     ret.append((tweet, []))
+                    continue
 
                 for key in media_keys:
                     media_objects = [m for m in media if key == m.media_key]
