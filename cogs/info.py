b4109fac6a5cf90aec7e616b72de463c34c90926
JDJG Inc. Official <ghostzap123@gmail.com>
2023-01-17 21:05:06 -0500

    That's try this again

--- cogs/info.py
+++ cogs/info.py
@@ -91,7 +91,8 @@ class Info(commands.Cog):
 
         user = user or interaction.user
 
-        ctx = self.bot.get_context(interaction)
+        ctx = await self.bot.get_context(interaction)
+        print(ctx)
 
         embed = discord.Embed(title=f"{user}", color=random.randint(0, 16777215), timestamp=ctx.message.created_at)
 
