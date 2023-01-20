381e5bad08ba00332e33842fb60bcfa748f51f75
JDJG Inc. Official <ghostzap123@gmail.com>
2023-01-20 15:41:33 -0500

    basic form of the laugh command is created

--- utils/image.py
+++ utils/image.py
@@ -70,3 +70,46 @@ def invert(image) -> discord.File:
         file = discord.File(f, "inv.gif")
 
     return file
+
+
+size = 220
+corner_offset = 10
+
+def laugh(asset) -> discord.File:
+  
+  f = BytesIO()
+
+  wrapped_image = BytesIO(asset)
+
+  frames = []
+  durations = []
+  
+  with Image.open("assets/images/laugh.png") as image:
+
+    with Image.new("RGBA", (image.width, image.height), "white") as canv:
+      with Image.open(wrapped_image) as custom_image:
+
+         loop = custom_image.info.get("loop", 0)
+          
+         if loop > 0:
+           print("loop isn't 0, but we will make sure it loops properly")
+
+         for frame in ImageSequence.Iterator(custom_image):
+            durations.append(frame.info.get("duration", 50))
+          
+            resized = frame.resize((size, size))
+            canv.paste(image, (0, 0))
+            canv.paste(resized, (corner_offset, canv.size[1] - ( size + corner_offset)))
+            frames.append(canv.copy())
+
+  if not custom_image.is_animated:
+     frames[0].save(f, format="PNG")
+     f.seek(0)
+     file = discord.File(f, "laugh.png")
+   
+  else:
+     frames[0].save(f, format="GIF", append_images=frames[1:], save_all=True, duration=durations, disposal=2, loop=0)
+     f.seek(0)
+     file = discord.File(f, "laugh.gif")
+
+  return file
