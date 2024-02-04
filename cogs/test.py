46621e87d0c02d7b33cdcc15ec7ca15ca1503cfd
JDJG <ghostzap123@gmail.com>
2024-02-04 00:43:13 -0500

    There I suppose?

--- cogs/test.py
+++ cogs/test.py
@@ -374,7 +374,15 @@ class Test(commands.Cog):
 
         await interaction.response.send_message(
             f"temperature :\nCelsius : {temps.celsius} \nFahrenheit: {temps.fahrenheight}  \nKelvin : {temps.kelvin}"
+
         )
+
+    @convert_temperature.error()
+    async def convert_temperature_error(self, interaction: discord.Interaction, error):
+        await interaction.response.send_message(f"{error}! Please Send to this to my developer", ephemeral=True)
+        print(interaction.command)
+        traceback.print_exc()
+        
 
 
 class Slash(commands.Cog):
