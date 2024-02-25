5cb3e0cf5fa15ee18317c48d14b10ab79b6db5d5
JDJG Inc. Official <ghostzap123@gmail.com>
2024-02-25 13:21:26 -0500

    smaller size now.

--- utils/image.py
+++ utils/image.py
@@ -216,7 +216,7 @@ def crusty(raw_assets: bytes) -> discord.File:
             img.coalesce()
             img.iterator_reset()
 
-        for d in (35, 2000):
+        for d in (35, 500):
             img.resize(d, d)
 
         img.save(file=f)
