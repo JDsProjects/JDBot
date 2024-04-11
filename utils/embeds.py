b77ba8b56819cd63c91d846987da7cd34d013c95
JDJG <ghostzap123@gmail.com>
2024-04-10 22:50:42 -0400

    There

--- utils/embeds.py
+++ utils/embeds.py
@@ -59,9 +59,9 @@ async def cdn_upload(bot, image_bytes):
 
     if isinstance(image_bytes, bytes):
         image_bytes = io.BytesIO(image_bytes)
+        image_bytes.seek(0)
 
     kind = filetype.guess(image_bytes)
-    image_bytes.seek(0)
 
     ext = kind.extension if kind else "gif"
 
