c52ffb88ed024ff5b20b783df88e0a79043a2077
JDJG <ghostzap123@gmail.com>
2024-05-03 14:25:23 -0400

    Fixed bugs

--- cogs/bot.py
+++ cogs/bot.py
@@ -521,7 +521,8 @@ class Bot(commands.Cog):
     async def suggest(self, ctx, *, args=None):
         if not args:
             return await ctx.send("You didn't give me a command to add to the suggestion.")
-            ctx.command.reset_cooldown(ctx)
+        
+        ctx.command.reset_cooldown(ctx)
 
         embed = discord.Embed(
             title=f"New Suggestion requested by {ctx.author}",
