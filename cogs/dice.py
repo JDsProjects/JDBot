830b92c086e74b4a3be80f45f35fe995f6fc691a
JDJG <ghostzap123@gmail.com>
2024-05-03 15:11:54 -0400

    There

--- cogs/dice.py
+++ cogs/dice.py
@@ -64,7 +64,8 @@ class Dice(commands.Cog):
                 if emoji_choice.available is False:
                     # idk why i keep on getting None though.
                     emoji_choice = emoji_choice.url
-
+        
+        emoji_choice = emoji_choice or "No Emoji Found"
         await ctx.send(f"{emoji_choosen} \n{emoji_choice}")
 
     @commands.command(brief="gives a random kawaii emoji.", aliases=["ka"])
