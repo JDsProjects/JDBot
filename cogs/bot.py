33248f9470bceea7055dee1bf91d4a2ea312d558
JDJG <ghostzap123@gmail.com>
2024-05-30 06:03:40 -0400

    add credits to person who offered a japanese translation
    Japanese translation for convert_temperature:
    Thank you: tp_link_z490
    
    name: tp-li
    
    
    (please don't dm me unprovoked thank you)

--- cogs/bot.py
+++ cogs/bot.py
@@ -698,6 +698,7 @@ class Bot(commands.Cog):
             910899642236043294,
             343019667511574528,
             1033906684965421086,
+            1163416356566347828,
         ]
         users = sorted([(f"{await self.bot.try_user(uid)}") or f"Unknown User#0000 ({uid})" for uid in to_credit])
         joined_users = "\n".join(users)
