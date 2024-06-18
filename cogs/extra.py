7e81845433b68433501efa805f617fe05598ceb4
JDJG <ghostzap123@gmail.com>
2024-06-18 04:29:01 -0400

    Made convert_temperature a guild_install in JDBot.

--- cogs/extra.py
+++ cogs/extra.py
@@ -1191,6 +1191,7 @@ class Extra(commands.Cog):
         await message.channel.send(msg, allowed_mentions=discord.AllowedMentions.none())
 
     @app_commands.user_install()
+    @app_commands.guild_install()
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
     @app_commands.command(description="A command to convert temperatures to different scales")
     async def convert_temperature(
